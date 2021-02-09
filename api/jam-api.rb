e31ccf4b4633c2143d89ff3c293ddbffead544a8
the-jen-wu <77735028+the-jen-wu@users.noreply.github.com>
2021-02-09 12:46:26 -0500

    Update jam-api.rb

--- api/jam-api.rb
+++ api/jam-api.rb
@@ -2,14 +2,16 @@ require 'sinatra'
 require 'aws-sdk-s3'
 require 'aws-sdk-sts'
 
-    get '/2' do
+get '/toggleRec' do
     `echo "POST /containers/jamulus/kill?signal=SIGUSR2 HTTP/1.0\r\n" | nc -U /tmp/docker.sock`
-    end
-    get '/1' do
-        `echo "POST /containers/jamulus/kill?signal=SIGUSR1 HTTP/1.0\r\n" | nc -U /tmp/docker.sock`
-      end
-   get '/sync' do
-       `do a thing to get the stuff`
-       credentials = aws.sts.client.get_token 
-        aws.s3.client.sync {credentials} file : bucket
-      end
+end
+
+get '/enableRec' do
+    `echo "POST /containers/jamulus/kill?signal=SIGUSR1 HTTP/1.0\r\n" | nc -U /tmp/docker.sock`
+end
+
+get '/sync' do
+    `do a thing to get the stuff`
+    credentials = aws.sts.client.get_token 
+    aws.s3.client.sync {credentials} file : bucket
+end
